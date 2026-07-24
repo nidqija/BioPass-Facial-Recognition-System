0ec105f87c7dfe5c452b44e9ed190f090682cd5b
Raziq Din <ajiqdin646@gmail.com>
2026-07-25 04:13:16 +0800

    clear boilerplate and add comments

--- server/floci-backend/config/floci_config.py
+++ server/floci-backend/config/floci_config.py
@@ -1,5 +1,6 @@
 import boto3
 
+# config for s3 client storage bucket
 s3 = boto3.client(
     "s3",
     endpoint_url="http://localhost:4566",
@@ -8,7 +9,7 @@ s3 = boto3.client(
     region_name="us-east-1",
 )
 
-
+# config for dynamodb client storage table
 dynamodb = boto3.client(
     "dynamodb",
      endpoint_url="http://localhost:4566",
@@ -17,6 +18,7 @@ dynamodb = boto3.client(
     region_name="us-east-1",
 )
 
+# config for floci url , bucket name and table name
 FLOCI_URL = "http://localhost:4566"
 BUCKET_NAME = "my-app-bucket"
 TABLE_NAME = "my-bucket-table"
